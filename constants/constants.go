2a5ac406f90b9ec625f0fd1c77e0992dfb2e2bf9
Chris Frank <chris@cfrank.org>
2016-10-06 22:20:06 +0300

    Well time to change the secrets... shit

--- constants/constants.go
+++
@@ -1,8 +0,0 @@
-/* This file includes constants which shouldn't be included included
- * in version control
- */
-package constants
-
-var HMAC_SECRET []byte = []byte("ch_8eth2$ubr6Tu?ed=P6c$6debraph5")
-
-const DB_HOST string = "tny:c85&_p2erustab#EheNEx5=w@tcp(127.0.0.1:3306)/tny"
